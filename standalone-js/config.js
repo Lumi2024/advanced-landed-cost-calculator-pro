496edfbc91af88ebd95ad3131bd2af4525b656e7
Lumi2024 <161085377+Lumi2024@users.noreply.github.com>
2026-03-03 15:25:51 +0100

    Update config.js

--- standalone-js/config.js
+++ standalone-js/config.js
@@ -1,10 +1,7 @@
 const CONFIG = {
-
     FX_API: "https://api.exchangerate.host/latest",
-
     BASE_CURRENCY: "EUR",
-
     VAT_DEFAULT: 20,
-
-    ENABLE_CHART: true
+    ENABLE_CHART: true,
+    APP_NAME: "Import Intelligence PRO"
 };
